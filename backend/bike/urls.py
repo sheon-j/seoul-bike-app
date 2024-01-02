a37820054ac7f411bd73346e0475e8faaa949e70
sheon-j <jsh7704@naver.com>
2024-01-02 22:46:36 +0900

    fix bike app list view urls

--- backend/bike/urls.py
+++ backend/bike/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import BikeList, BikeDetail
 
 urlpatterns = [
-    path(route='bike', view=BikeList.as_view(), name='bike_list'),
+    path(route='bike/', view=BikeList.as_view(), name='bike_list'),
     path(route='bike/<int:id>', view=BikeDetail.as_view(), name='bike_detail'),
 ]
